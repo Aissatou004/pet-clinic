4e0fb74a562421f38a35a39c2d9d82a6c060b695
fatoumory <fcndong@ept.sn>
2026-08-19 11:29:58 +0000

    [G6] Appel API Animal

--- src/main/webapp/app/home/home.ts
+++ src/main/webapp/app/home/home.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 import { AccountService } from 'app/core/auth/account.service';
+import { AnimalService } from 'app/entities/animal/service/animal.service';
 import { TranslateDirective } from 'app/shared/language';
 
 @Component({
@@ -9,14 +11,31 @@ import { TranslateDirective } from 'app/shared/language';
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.html',
   styleUrl: './home.scss',
-  imports: [TranslateDirective, RouterLink],
+  imports: [CommonModule],
 })
-export default class Home {
-  public readonly account = inject(AccountService).account;
+export default class Home implements OnInit {
+  private readonly animalService = inject(AnimalService);
 
-  private readonly router = inject(Router);
+  totalAnimals = signal<number | null>(null);
+  isLoadingAnimals = signal<boolean>(true);
 
-  login(): void {
-    this.router.navigate(['/login']);
+  ngOnInit(): void {
+    this.loadTotalAnimals();
+  }
+
+  loadTotalAnimals() {
+    this.animalService.countTotal().subscribe({
+      next: res => {
+        const totalHeaders = res.headers.get('X-Total-Count');
+        const count = totalHeaders ? parseInt(totalHeaders, 10) : (res.body?.length ?? 0);
+
+        this.totalAnimals.set(count);
+        this.isLoadingAnimals.set(false);
+      },
+      error: () => {
+        this.totalAnimals.set(0);
+        this.isLoadingAnimals.set(false);
+      },
+    });
   }
 }
