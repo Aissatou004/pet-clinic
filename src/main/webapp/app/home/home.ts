c5672b2e701c3a647d24f98e58771d9ec16f6f35
fatoumory <fcndong@ept.sn>
2026-08-19 13:12:04 +0000

    [G6] Affichage  rendez vous du jour

--- src/main/webapp/app/home/home.ts
+++ src/main/webapp/app/home/home.ts
@@ -1,9 +1,11 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 import { AccountService } from 'app/core/auth/account.service';
 import { AnimalService } from 'app/entities/animal/service/animal.service';
+import { IRendezVous } from 'app/entities/rendez-vous/rendez-vous.model';
+import { RendezVousService } from 'app/entities/rendez-vous/service/rendez-vous.service';
 import { TranslateDirective } from 'app/shared/language';
 
 @Component({
@@ -11,16 +13,22 @@ import { TranslateDirective } from 'app/shared/language';
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.html',
   styleUrl: './home.scss',
-  imports: [CommonModule],
+  imports: [CommonModule, DatePipe],
 })
 export default class Home implements OnInit {
   private readonly animalService = inject(AnimalService);
+  private readonly rendezVousService = inject(RendezVousService);
 
   totalAnimals = signal<number | null>(null);
   isLoadingAnimals = signal<boolean>(true);
 
+  totalRendezVous = signal<number | null>(null);
+  rendezVousToday = signal<IRendezVous[]>([]);
+  isLoadingRendezVous = signal<boolean>(true);
+
   ngOnInit(): void {
     this.loadTotalAnimals();
+    this.loadRendezVous();
   }
 
   loadTotalAnimals() {
@@ -35,6 +43,22 @@ export default class Home implements OnInit {
       error: () => {
         this.totalAnimals.set(0);
         this.isLoadingAnimals.set(false);
+      },
+    });
+  }
+
+  loadRendezVous() {
+    this.rendezVousService.rendezVousToday().subscribe({
+      next: res => {
+        console.log('Données reçues du serveur :', res);
+        this.totalRendezVous.set(res.length);
+        this.rendezVousToday.set(res);
+        this.isLoadingRendezVous.set(false);
+      },
+      error: () => {
+        this.totalRendezVous.set(0);
+        this.rendezVousToday.set([]);
+        this.isLoadingRendezVous.set(false);
       },
     });
   }
